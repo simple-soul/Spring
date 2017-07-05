e4d459fb076d0d652defcea532db90dcc37480c0
simple-soul <8146626382qq.com>
2017-07-05 22:53:47 +0800

    jdbc1

--- src/main/kotlin/com/kotlin/Dao/Dao.kt
+++ src/main/kotlin/com/kotlin/Dao/Dao.kt
@@ -1,6 +1,7 @@
 package com.kotlin.Dao
 
 import com.kotlin.annotations.Bean
+import org.junit.Test
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.datasource.DriverManagerDataSource
 import org.springframework.stereotype.Component
@@ -11,18 +12,25 @@ import org.springframework.stereotype.Component
 
 @Bean
 @Component(value = "studentDao")
-data class StudentDao(var name:String)
+class StudentDao
 {
+    @Test
     fun add()
     {
+        //设置数据库信息
         var dm = DriverManagerDataSource()
         dm.setDriverClassName("com.mysql.jdbc.Driver")
-        dm.url = "jdbc:mysql///test"
+        dm.url = "jdbc:mysql://localhost:3306/test"
         dm.username = "root"
         dm.password = "gg123456"
 
+        //创建JdbcTemplate对象，设置数据源
         var template = JdbcTemplate(dm)
 
-        println("add in dao")
+        //使用JdbcTemplate对象的方法操作数据
+        var sql = "insert into student value(?, ?)"
+        val row = template.update(sql, "小黄", "fsfd")
+
+        println("add $row")
     }
 }
